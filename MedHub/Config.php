b9b33bd7336a986e8e963c65100017e86414c278
Tim Windelschmidt <fionera@fionera.de>
2017-06-28 09:50:59 +0200

    New Name

--- MedHub/Config.php
+++ MedHub/Config.php
@@ -66,7 +66,7 @@ class Config
          * Adding some Basic Information about the Website
          */
         $this->rawConfig += [
-            "pageName" => "MedddHubbb",
+            "pageName" => "RUSH B",
             "pageSlogan" => [
                 "de" => "Einfacher Medikamente organisieren",
                 "en" => "Easier Medicine management"
